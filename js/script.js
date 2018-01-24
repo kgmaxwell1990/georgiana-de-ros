9bc9001a5add0be298bc0a57863614b5774f03a8
Katie Maxwell <katiemaxwell10@hotmail.com>
2018-01-24 20:02:37 +0000

    styling of home page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
+$(window).on("load",function() {
 $(function() {
     $('.nav a').on('click', function(){ 
         if($('.navbar-toggle').css('display') !='none'){
             $(".navbar-toggle").trigger( "click" );
         }
     });
+});
+
+$(".slide-row-1").fadeIn(1000);
+$(".slide-row-2").delay( 400 ).fadeIn( 1000 );
+$(".slide-row-3").delay( 800 ).fadeIn( 1000 );
+
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".slide-row").each(function() {
+        /* Check the location of each desired element */
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+        } else { //object goes out of view (scrolling up)
+          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+  
+
+
 });
